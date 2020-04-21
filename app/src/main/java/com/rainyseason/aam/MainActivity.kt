c33a33b3ceaf29f6e2c42f688f3b74e84e2868b8
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2020-04-22 00:33:32 +0700

    circle ci: test check apk size

--- app/src/main/java/com/rainyseason/aam/MainActivity.kt
+++ app/src/main/java/com/rainyseason/aam/MainActivity.kt
@@ -24,5 +24,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        hello()
+    }
+
+    private fun hello() {
+        println("Hello!")
     }
 }
