b5d61f10b4906132bb850c4bdb4e64e04bf773e9
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2020-04-21 17:37:55 +0700

    circle ci: hello world on google/cloud-sdk

--- scripts/check_apk_size.sh
+++ scripts/check_apk_size.sh
@@ -1,1 +1,6 @@
 echo "hello world"
+echo "$AAM_GCLOUD_SERVICE_ACCOUNT_KEY_BASE64" | base64 -d -i - >> "key.json"
+gcloud auth activate-service-account all-about-min@appspot.gserviceaccount.com --key-file=./key.json
+gcloud config set project all-about-min
+gsutil cp gs://all-about-min.appspot.com/11080425_455425407954554_5715761550478109524_o.jpg ./image.jpg
+ls -lha
