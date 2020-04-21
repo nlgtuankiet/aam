0b368d6108962f8ff173bbab7f018f3e65c0e6b4
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2020-04-21 17:57:29 +0700

    found master commit

--- scripts/check_apk_size.sh
+++ scripts/check_apk_size.sh
@@ -1,6 +1,34 @@
-echo "hello world"
-echo "$AAM_GCLOUD_SERVICE_ACCOUNT_KEY_BASE64" | base64 -d -i - >> "key.json"
-gcloud auth activate-service-account all-about-min@appspot.gserviceaccount.com --key-file=./key.json
-gcloud config set project all-about-min
-gsutil cp gs://all-about-min.appspot.com/11080425_455425407954554_5715761550478109524_o.jpg ./image.jpg
-ls -lha
+# echo "hello world"
+# echo "$AAM_GCLOUD_SERVICE_ACCOUNT_KEY_BASE64" | base64 -d -i - >> "key.json"
+# gcloud auth activate-service-account all-about-min@appspot.gserviceaccount.com --key-file=./key.json
+# gcloud config set project all-about-min
+# gsutil cp gs://all-about-min.appspot.com/11080425_455425407954554_5715761550478109524_o.jpg ./image.jpg
+# echo "$CIRCLE_BRANCH"
+
+compare_apk() {
+  masterBranchRegex="master"
+  current_commit_sha=$(git rev-parse HEAD)
+    echo "Compare apk"
+  fromCommit=$(git log -n 1 --pretty=format:%H HEAD~0)
+  masterCommit=""
+  masterCommitBranches=""
+
+  number=0
+  while :
+  do
+    masterCommit=$(git log -n 1 --pretty=format:%H HEAD~$number)
+    masterCommitBranches=$(git branch --quiet -r --contains "${masterCommit}" | cat)
+    echo "checking: $masterCommit"
+    echo "branches: $masterCommitBranches"
+    if [[ "${masterCommitBranches}" =~ ${masterBranchRegex} ]]; then
+      echo "branches: $masterCommitBranches"
+      echo "found master commit: ${masterCommit}"
+      break
+    fi
+    number=$((number+1))
+  done
+}
+
+compare_apk
+
+
