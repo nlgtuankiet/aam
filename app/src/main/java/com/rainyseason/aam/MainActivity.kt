30aaac8d60539f8074fcec75216a8eb735a81e56
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2020-04-22 17:35:37 +0700

    circle ci: lint warning as error

--- app/src/main/java/com/rainyseason/aam/MainActivity.kt
+++ app/src/main/java/com/rainyseason/aam/MainActivity.kt
@@ -17,13 +17,17 @@
 package com.rainyseason.aam
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
             setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.sample)
+        textView.text = """123"""
                                 hello()
     }
 
